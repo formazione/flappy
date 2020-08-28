82069d7a31f05528e8dea2d511cf942974cb81b8
formazione <gatto.gio@gmail.com>
2020-08-28 08:59:52 +0200

    5th tutorial (last one) with audio, score and splash screen

--- flappy5.py
+++ flappy5.py
@@ -1,10 +1,49 @@
 import pygame
 from glob import glob
 import random
+from functions.score import *
 
+#
+# - added sound
+# - added font and score on screen
+# - save record
+#
+
+pygame.init()
+pygame.font.init()
 size = w, h = 400, 600
-screen = pygame.display.set_mode((size))
+screen = pygame.display.set_mode((size), 32, 1)
 pygame.display.set_caption("Flappy Py")
+# 
+game = Score("score.txt")
+# ================== MUSIC ===================== #
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
+pygame.mixer.quit()
+pygame.mixer.init(44100, -16, 2, 512)
+pygame.mixer.set_num_channels(32)
+
+# ===============================================
+jump = pygame.mixer.Sound("sounds/jump.wav")
+jump.set_volume(0.5)
+hit = pygame.mixer.Sound("sounds/hit.wav")
+hit.set_volume(0.3)
+# To play a sound use: play(jump)
+
+
+def play(snd):
+    "Plays one of the sounds in the sounds folder using play('name')"
+    pygame.mixer.Sound.play(snd)
+# ============================================== #
+
+
+font = pygame.font.SysFont("Arial", 16)
+
+
+def write(text_to_show, color="Coral"):
+    'write("Start")'
+    text = font.render(text_to_show, 1, pygame.Color(color))
+    return text
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -15,11 +54,15 @@ class Sprite(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.frames = [load(f[:-4]) for f in glob(file + "*.png")]
+        self.framesup = [rotate(f[:-4], 45) for f in glob(file + "*.png")]
         self.image = self.frames[0]
-        self.imagefall = load("fall")
-        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.imagefall = rotate("fall", -45)
+        self.imagespeed = load("speed")
+
+        self.rect = pygame.Rect(self.x, self.y, 32, 24)
         self.cnt = 0
         self.score = 0
+        self.maxscore = int(game.maxscore)
         self.mask = self.make_mask()
         g.add(self)
 
@@ -29,23 +72,29 @@ class Sprite(pygame.sprite.Sprite):
     def update(self):
         global moveup, gameover
         # when moveup animation's faster
-        if not moveup:
-            self.cnt += .1
+        self.cnt += .1
         if self.cnt > len(self.frames) - 1:
             self.cnt = 0
-        self.image = self.frames[int(self.cnt)]
+        if not moveup:
+            self.image = self.frames[int(self.cnt)]
+        else:
+            self.image = self.framesup[int(self.cnt)]
         if not gameover:
             self.score += 1
-        pygame.display.set_caption(str(self.score))
         self.check_collision()
+
 
     def check_collision(self):
         global gameover
 
         for pipe in pipes:
             if pygame.sprite.collide_mask(pipe, self):
-                print("touched")
+                if gameover == 0:
+                    play(hit)
+                    if self.score > self.maxscore:
+                        game.save_score(self.score)
                 gameover = 1
+
 
 
 
@@ -71,6 +120,7 @@ class Pipe(pygame.sprite.Sprite):
         self.x = x + 300
         self.pos = pos
         self.y = random.randint(300, 400)
+        self.speed = 1
 
         if self.pos == 0:
             self.image = load(file)
@@ -78,21 +128,22 @@ class Pipe(pygame.sprite.Sprite):
         else:
             self.image = flip(file)
             self.rect = pygame.Rect(self.x, self.y, 52, 320)
-            self.rect.bottom = self.y - random.randint(200, 300)
+            self.rect.bottom = self.y - random.randint(150, 300)
         pipes.add(self)
         g.add(self)
         self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
 
-        self.rect.left -= 1
+        self.rect.left -= self.speed
         if self.rect.left < -100:
             self.rect.left = 400
             self.y = random.randint(300, 400)
             if self.pos == 0:
                 self.rect.top = self.y
             else:
-                self.rect.bottom = self.y - random.randint(200, 300)
+                # DISTANCE AMONG PIPES
+                self.rect.bottom = self.y - random.randint(150 - flappy.score // 100, 300 - flappy.score // 100)
 
 
 
@@ -105,10 +156,11 @@ class Base(pygame.sprite.Sprite):
         self.y = y
         self.image = load(file)
         self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.speed = 1
         g.add(self)
 
     def update(self):
-        self.rect.left -= 1
+        self.rect.left -= self.speed
         if self.rect.left < -400:
             self.rect.left = 399
 
@@ -121,6 +173,10 @@ def flip(file):
     return pygame.transform.flip(load(file), 0, 1)
 
 
+def rotate(file, angle):
+    return pygame.transform.rotate(load(file), angle)
+
+
 def gravity(sprite):
     sprite.rect.top += 1
 
@@ -128,7 +184,7 @@ def gravity(sprite):
 
 
 def start():
-    global g, pipes, flappy
+    global g, pipes, flappy, b1, b2
 
     g = pygame.sprite.Group()
     Bg("bg", 0, 0)
@@ -145,15 +201,15 @@ def start():
     Pipe("pipe", 400, 0, 1)
     Pipe("pipe", 500, 0, 1)
 
-    Base("base", 400, 570)
-    Base("base", 0, 570)
+    b1 = Base("base", 400, 570)
+    b2 = Base("base", 0, 570)
     flappy = Sprite("blue", 50, 300)
     main()
 
 
 def main():
     global moveup, gameover
-    global g, pipes, flappy
+    global g, pipes, flappy, b1, b2
 
     # jump controlo variables:
     # - after you press
@@ -167,12 +223,26 @@ def main():
     topjump = 30
     # how speed it jumps
     jumpspeed = 2
-    screen.fill((0,0,0))
-    pygame.init()
-    pygame.font.init()
+    screen.fill((0, 0, 0))
+    # ============ p.5
+    text = write("Score")
     clock = pygame.time.Clock()
     loop = 1
+    speedup = 0
+
     while loop:
+        if speedup:
+            b1.speed = 10
+            b2.speed = 10
+            for pipe in pipes:
+                pipe.speed = 10
+                moveup = 1
+            flappy.image = flappy.imagespeed
+        else:
+            b1.speed = 1
+            b2.speed = 1
+            for pipe in pipes:
+                pipe.speed = 1
 
         if moveup:
             flappy.rect.top -= jumpspeed
@@ -191,23 +261,36 @@ def main():
             flappy.rect.top += 1
             flappy.cnt = 0
             flappy.image = flappy.imagefall
-
+            if flappy.rect.top > 600:
+                flappy.kill()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = 0
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
+                    play(jump)
                     moveup = 1
                     startcounter = 1
+                if event.key == pygame.K_RIGHT:
+                    play(jump)
+                    speedup = 1
                 if event.key == pygame.K_ESCAPE:
                     loop = 0
-                if event.key == pygame.K_s:
-                    start()
-
+                if event.key == pygame.K_m:
+                    menu()
+            if event.type == pygame.KEYUP:
+                moveup = 0
+                speedup = 0
 
         g.draw(screen)
         g.update()
+        
+        screen.blit(flappy.image, (0, 0))
+        screen.blit(text, (50, 0))
+        screen.blit(write(str(flappy.score), color="White"), (100, 0))
+        screen.blit(write(str(flappy.maxscore), color="White"), (150, 0))
+
         if not moveup:
             gravity(flappy)
         pygame.display.update()
@@ -216,6 +299,30 @@ def main():
     pygame.quit()
 
 
-start()
+def menu():
+    "This is the menu that waits you to click the s key to start"
+    bb = pygame.image.load("bg.png")
+    fl = pygame.image.load("bluebird-downflap.png")
+    screen.blit(bb, (0, 0))
+    screen.blit(fl, (100, 300))
+    loop1 = 1
+    screen.blit(write("Flappy Pygame"), (10, 0))
+    screen.blit(write("Press any Key"), (10, 50))
+    screen.blit(write("Press m to come back to this menu"), (10, 80))
+    screen.blit(write("Press arrow key up to fly up and arrow key right to fly fast"), (10, 200))
+    while loop1:
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                loop1 = 0
+            if event.type == pygame.KEYDOWN:
+                press_escape = event.key == pygame.K_ESCAPE
+                if press_escape:
+                    loop1 = 0
+
+                start()
+        pygame.display.update()
+
+    pygame.quit()
 
 
+menu()
