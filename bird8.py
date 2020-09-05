0434f8ad504b14d14aece3f307a5508afba9a709
formazione <gatto.gio@gmail.com>
2020-09-05 18:27:09 +0200

    added touch control or mouse control

--- bird8.py
+++ bird8.py
@@ -11,9 +11,9 @@ from functions.score import *
 
 pygame.init()
 pygame.font.init()
-size = w, h = 400, 600
+size = w, h = 800, 600
 screen = pygame.display.set_mode((size), 32, 1)
-pygame.display.set_caption("Flappy Py")
+pygame.display.set_caption("Yellow bird")
 # 
 game = Score("score.txt")
 # ================== MUSIC ===================== #
@@ -75,11 +75,11 @@ class Sprite(pygame.sprite.Sprite):
 
         self.x = x
         self.y = y
-        self.frames = [load(f[:-4]) for f in glob(file + "*.png")]
-        self.framesup = [rotate(f[:-4], 45) for f in glob(file + "*.png")]
+        self.frames = [load(f[:-4]) for f in glob("imgs2\\" + file + "*.png")]
+        self.framesup = [load(f[:-4]) for f in glob("imgs2\\" + file + "*.png")]
         self.image = self.frames[0]
-        self.imagefall = rotate("fall", -45)
-        self.imagespeed = load("speed")
+        self.imagefall = rotate("imgs2\\blue4", -45)
+        self.imagespeed = load("imgs2\\blue4")
 
         self.rect = pygame.Rect(self.x, self.y, 32, 24)
         self.cnt = 0
@@ -94,7 +94,7 @@ class Sprite(pygame.sprite.Sprite):
     def update(self):
         global moveup, gameover
         # when moveup animation's faster
-        self.cnt += .1
+        self.cnt += .05
         if self.cnt > len(self.frames) - 1:
             self.cnt = 0
         if not moveup:
@@ -103,7 +103,7 @@ class Sprite(pygame.sprite.Sprite):
             self.image = self.framesup[int(self.cnt)]
         if not gameover:
             self.score += 1
-        self.check_collision()
+        #self.check_collision()
 
 
     def check_collision(self):
@@ -177,14 +177,14 @@ class Base(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.image = load(file)
-        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.rect = pygame.Rect(self.x, self.y, 800, 32)
         self.speed = 1
         g.add(self)
 
     def update(self):
         self.rect.left -= self.speed
-        if self.rect.left < -400:
-            self.rect.left = 399
+        if self.rect.left < -800:
+            self.rect.left = 799
 
 
 def load(file):
@@ -209,40 +209,48 @@ def start():
     global g, pipes, flappy, b1, b2
 
     g = pygame.sprite.Group()
-    Bg("bg", 0, 0)
+    Bg("bg3", 0, 0)
     pipes = pygame.sprite.Group()
-    Pipe("pipe", 100, 300, 0)
-    Pipe("pipe", 200, 300, 0)
-    Pipe("pipe", 300, 300, 0)
-    Pipe("pipe", 400, 300, 0)
-    Pipe("pipe", 500, 300, 0)
-    # Upside down
-    Pipe("pipe", 100, 0, 1)
-    Pipe("pipe", 200, 0, 1)
-    Pipe("pipe", 300, 0, 1)
-    Pipe("pipe", 400, 0, 1)
-    Pipe("pipe", 500, 0, 1)
+    # Pipe("pipe2", 100, 300, 0)
+    # Pipe("pipe2", 200, 300, 0)
+    # Pipe("pipe2", 300, 300, 0)
+    # Pipe("pipe2", 400, 300, 0)
+    # Pipe("pipe2", 500, 300, 0)
 
-    b1 = Base("base", 400, 570)
-    b2 = Base("base", 0, 570)
+
+    b2 = Base("base3", 0, 500)
+    b1 = Base("base3", 800, 500)
     flappy = Sprite("blue", 50, 300)
     main()
 
+def moveupndown():
+    global movedown, moveup, startcounter, jumpspeed
+
+    if moveup:
+        flappy.rect.top -= jumpspeed
+        # wing animation speed
+        flappy.cnt += .02
+        # print(startcounter)
+
+    if movedown:
+        flappy.rect.top += jumpspeed
+        # fly faster
+        flappy.cnt += .02
+
+
 
 def main():
-    global moveup, gameover
     global g, pipes, flappy, b1, b2
-
+    global movedown, moveup, startcounter, jumpspeed
+    global gameover
     # jump controlo variables:
     # - after you press
     moveup = 0
+    movedown = 0
 
     # =================== tutorial 4 - change 1
     gameover = 0
     # how high can go
-    startcounter = 0
-    # How hight flappy jumps
-    topjump = 30
     # how speed it jumps
     jumpspeed = 2
     screen.fill((0, 0, 0))
@@ -250,35 +258,11 @@ def main():
     text = write("Score")
     clock = pygame.time.Clock()
     loop = 1
-    speedup = 0
-
     while loop:
 
         if flappy.score % 2500 == 0:
             soundtrack()
-
-        if speedup:
-            b1.speed = 10
-            b2.speed = 10
-            for pipe in pipes:
-                pipe.speed = 10
-                moveup = 1
-            flappy.image = flappy.imagespeed
-        else:
-            b1.speed = 1
-            b2.speed = 1
-            for pipe in pipes:
-                pipe.speed = 1
-
-        if moveup:
-            flappy.rect.top -= jumpspeed
-            startcounter += 1
-            # fly faster
-            flappy.cnt += .5
-            # print(startcounter)
-        if startcounter == topjump:
-            startcounter = 0
-            moveup = 0
+        moveupndown()
 
         if gameover:
             # cannot fly
@@ -293,85 +277,22 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 loop = 0
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                play(jump)
-                moveup = 1
-                startcounter = 1
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    play(jump)
+                    # play(jump)
                     moveup = 1
+                    jumpspeed = 2
                     startcounter = 1
-                if event.key == pygame.K_RIGHT:
-                    play(jump)
-                    speedup = 1
+                if event.key == pygame.K_DOWN:
+                    movedown = 1
                 if event.key == pygame.K_ESCAPE:
                     loop = 0
                 if event.key == pygame.K_m or event.key == pygame.K_s:
                     menu()
             if event.type == pygame.KEYUP:
                 moveup = 0
-                speedup = 0
-            if event.type == pygame.MOUSEBUTTONUP:
-                moveup = 0
-
-
-        g.draw(screen)
-        g.update()
-        
-        screen.blit(flappy.image, (0, 0))
-        screen.blit(text, (50, 0))
-        screen.blit(write(str(flappy.score), color="White"), (100, 0))
-        screen.blit(write(str(flappy.maxscore), color="White"), (150, 0))
-
-        if not moveup:
-            gravity(flappy)
-        pygame.display.update()
-        clock.tick(120)
-
-    pygame.quit()
-
-
-def menu():
-
-    "This is the menu that waits you to click the s key to start"
-    bb = pygame.image.load("bg.png")
-    fl = pygame.image.load("bluebird-downflap.png")
-    screen.blit(bb, (0, 0))
-    screen.blit(fl, (100, 300))
-    screen.blit(write("Flappy Pygame"), (10, 0))
-    screen.blit(write("Press any Key"), (10, 50))
-    screen.blit(write("Press m to come back to this menu"), (10, 80))
-    screen.blit(write("Press arrow key up to fly up and arrow key right to fly fast"), (10, 200))
+                movedown = 0
     
-    soundtrack()
-
-    loop1 = 1
-    while loop1:
-        for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
-                loop1 = 0
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                play(jump)
-                moveup = 1
-                startcounter = 1
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    play(jump)
-                    moveup = 1
-                    startcounter = 1
-                if event.key == pygame.K_RIGHT:
-                    play(jump)
-                    speedup = 1
-                if event.key == pygame.K_ESCAPE:
-                    loop = 0
-                if event.key == pygame.K_m or event.key == pygame.K_s:
-                    menu()
-            if event.type == pygame.KEYUP:
-                moveup = 0
-                speedup = 0
-            if event.type == pygame.MOUSEBUTTONUP:
-                moveup = 0
 
         g.draw(screen)
         g.update()
@@ -381,8 +302,8 @@ def menu():
         screen.blit(write(str(flappy.score), color="White"), (100, 0))
         screen.blit(write(str(flappy.maxscore), color="White"), (150, 0))
 
-        if not moveup:
-            gravity(flappy)
+        # if not moveup:
+        #     gravity(flappy)
         pygame.display.update()
         clock.tick(120)
 
@@ -392,8 +313,8 @@ def menu():
 def menu():
 
     "This is the menu that waits you to click the s key to start"
-    bb = pygame.image.load("bg.png")
-    fl = pygame.image.load("bluebird-downflap.png")
+    bb = pygame.image.load("bg3.png")
+    fl = pygame.image.load("imgs\\blue1.png")
     screen.blit(bb, (0, 0))
     screen.blit(fl, (100, 300))
     screen.blit(write("Flappy Pygame"), (10, 0))
